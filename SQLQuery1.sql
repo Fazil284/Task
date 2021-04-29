f4b859b51a5fbbf40edc9ebc091876169cfeb871
Fazil284 <82799132+Fazil284@users.noreply.github.com>
2021-04-29 18:00:53 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,40 @@
+-1
+select city,count(au_fname) as Count_Author  from authors 
+group by(city);
+
+--2
+select au_fname,au_lname from authors where city not in( 
+select city from publishers where pub_name='New Moon Books')
+
+--3
+create proc proc_update(@a_fname varchar(20),@a_lname varchar(20),@price float)
+as
+begin
+	update titles set price = @price where title_id in(
+	select title_id from titleauthor where au_id in(
+	select au_id from authors where au_fname=@a_fname and au_lname=@a_lname))
+end
+
+exec proc_update "Johnson","White",12.8
+
+--4
+create function fn_Tax(@qty int)
+returns float
+as 
+begin
+declare 
+	@tax float
+set @tax = 0
+if(@qty>0 and @qty< 10)
+	set @tax = 2
+else if(@qty >= 10 and @qty <20)
+	set @tax = 5
+else if(@qty >=20 and @qty < 30)
+	set @tax = 6
+else
+	set @tax = 7.5
+return @tax
+end
+select title_id,qty,dbo.fn_Tax(qty) as Tax from sales;
+
+select * from sales
