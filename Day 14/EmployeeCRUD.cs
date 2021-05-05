a49d909b40e9f34fe4148541626ee288a78a193a
Fazil284 <82799132+Fazil284@users.noreply.github.com>
2021-05-05 16:29:43 +0530

    Add files via upload

---
+++ Day 14/EmployeeCRUD.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransportManagementBLLibrary;
+
+namespace TransportManagementFEProject
+{
+    class EmployeeCRUD
+    {
+        IRepo<Employee> repo;
+        public EmployeeCRUD() { }
+        public EmployeeCRUD(IRepo<Employee> repo)
+        {
+            this.repo = repo;
+        }
+        public void AddEmployee()
+        {
+            Employee employee = new Employee();
+            employee.GetEmployeeDetails();
+            repo.Add(employee);
+        }
+        public void PrintAllEmployees()
+        {
+            var employees = repo.GetAll();
+            try
+            {
+                foreach (var item in employees)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Empty record");
+            }
+
+        }
+        public void DeleteEmployee()
+        {
+            Console.WriteLine("Please enter the employee Id to delete");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Employee employee = repo.Get(id);
+            if (employee == null)
+                Console.WriteLine("Cannot delete .No such employee");
+            else
+                repo.Delete(id);
+        }
+    }
+}
