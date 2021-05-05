5abadd9e067d46cccc39951a5e2f728524b664f5
Fazil284 <82799132+Fazil284@users.noreply.github.com>
2021-05-05 16:29:12 +0530

    Add files via upload

---
+++ Day 14/UnableToAddEmployeeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportManagementBLLibrary
+{
+    public class UnableToAddEmployeeException : ApplicationException
+    {
+        string _message;
+        public UnableToAddEmployeeException()
+        {
+            _message = "Unable to add employee cos of Id duplication. Tryagain.";
+        }
+        public override string Message => _message;
+    }
+}
