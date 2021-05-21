9554c2c645594e1adc91223d6f8b926e84ced50e
Fazil284 <82799132+Fazil284@users.noreply.github.com>
2021-05-21 22:11:34 +0530

    Add files via upload

---
+++ Task-4/Profile Task/Models/Prof.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task4.Models
+{
+    public class Prof
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Qualification { get; set; }
+        public string IsEmployed { get; set; }
+        public int NoticePeriod { get; set; }
+        public double CurrentCTC { get; set; }
+    }
+}
