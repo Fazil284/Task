86a6da9ccd9ed5253a56b71d43fc31006b9547f1
Fazil284 <82799132+Fazil284@users.noreply.github.com>
2021-05-21 23:03:21 +0530

    Add files via upload

---
+++ Task-4/Weather Task/Migrations/20210521130829_init.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task4b.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Weathers",
+                columns: table => new
+                {
+                    City = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    highTemp = table.Column<double>(type: "float", nullable: false),
+                    lowTemp = table.Column<double>(type: "float", nullable: false),
+                    forecast = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weathers", x => x.City);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Weathers");
+        }
+    }
+}
