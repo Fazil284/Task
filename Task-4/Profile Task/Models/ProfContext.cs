68541c27d1d595a45616d8f734d11ae5a0110f7a
Fazil284 <82799132+Fazil284@users.noreply.github.com>
2021-05-21 22:12:23 +0530

    Add files via upload

---
+++ Task-4/Profile Task/Models/ProfContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task4.Models
+{
+    public class ProfContext:DbContext
+    {
+        public ProfContext():base()
+        {
+
+        }
+        public ProfContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Prof> Profs { get; set; }
+    }
+}
