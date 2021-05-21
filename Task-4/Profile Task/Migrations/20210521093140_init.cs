38670823cf6bb4b13ca91ecae9ce5cb8b30f2d1a
Fazil284 <82799132+Fazil284@users.noreply.github.com>
2021-05-21 22:41:53 +0530

    Add files via upload

---
+++ Task-4/Profile Task/Migrations/20210521093140_init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task4.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Qualification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsEmployed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoticePeriod = table.Column<int>(type: "int", nullable: false),
+                    CurrentCTC = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profs");
+        }
+    }
+}
